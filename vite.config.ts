0f2b4bbf99f5d500144b9af85a8715f7fb231058
KyleTryon <git@techsquidtv.com>
2025-05-06 11:56:14 -0400

    chore: fmt

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(),],
-  build: {
-    ssr: 'src/app.tsx',
-    outDir: 'dist',
-    target: 'node20',
-    emptyOutDir: true,
-    sourcemap: true,
-    minify: false,      // keep it readable for now
-    rollupOptions: {
-      output: {
-        /**
-         * Produce a single file called app.js
-         * plus a shebang so users can call `blenderq` directly
-         */
-        entryFileNames: 'app.js',
-        format: 'esm',
-        banner: '#!/usr/bin/env node',
-      },
+    plugins: [react(), tsconfigPaths()],
+    build: {
+        ssr: 'src/app.tsx',
+        outDir: 'dist',
+        target: 'node20',
+        emptyOutDir: true,
+        sourcemap: true,
+        minify: false, // keep it readable for now
+        rollupOptions: {
+            output: {
+                /**
+                 * Produce a single file called app.js
+                 * plus a shebang so users can call `blenderq` directly
+                 */
+                entryFileNames: 'app.js',
+                format: 'esm',
+                banner: '#!/usr/bin/env node',
+            },
+        },
     },
-  },
 })
